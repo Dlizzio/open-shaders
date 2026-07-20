acaa06bf48728d171e1ec6f4a820c777b4942c54
Dlizzio <77717521+Dlizzio@users.noreply.github.com>
2026-07-20 13:55:34 -0700

    init

--- src/State.cpp
+++ src/State.cpp
@@ -115,8 +115,6 @@ void State::Draw()
 		if (currentShader && updateShader) {
 			if (currentShader->shaderType.get() == RE::BSShader::Type::Utility) {
 				if (currentPixelDescriptor & static_cast<uint32_t>(SIE::ShaderCache::UtilityShaderFlags::RenderShadowmask)) {
-					if (volumetricShadows.loaded)
-						volumetricShadows.CopyShadowLightData();
 					if (globals::features::exponentialHeightFog.loaded)
 						globals::features::exponentialHeightFog.CaptureDirectionalShadowMap();
 				}
